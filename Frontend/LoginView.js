8d4186974da1ea4a7ede00d36cbd59a3f8674b64
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-03-01 09:31:13 -0500

    feat: login functionality

--- Frontend/LoginView.js
+++ Frontend/LoginView.js
@@ -7,10 +7,42 @@ export default class LoginView {
     this.loading = false;
   }
 
-  async render(){
-    console.log('entra')
+  async render() {
     const app = document.getElementById("app");
-    const mainContent = await fetch (`./../Frontend/pages/login.html`).then(r => r.text())
+    const mainContent = await fetch(`./../Frontend/pages/login.html`).then(
+      (r) => r.text(),
+    );
     app.innerHTML = mainContent;
+
+    this.attachEventHandlers();
+  }
+
+  attachEventHandlers() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+
+      const response = await fetch(`http://localhost:1115/user?email=${email}`);
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(errorData);
+        alert("user not found");
+        return;
+      }
+
+      const data = await response.json();
+
+      if (data.length > 0) {
+        if (password == data[0].password) {
+          alert("nice");
+        } else {
+          alert("bad auth");
+        }
+      } else {
+        alert("user not found");
+      }
+    });
   }
 }
